b2b05598597ba932162ba86fa2f72389253f04d6
loannvitorinfo <loannvitor@gmail.com>
2022-11-29 16:39:34 -0300

    Tela de registro Tela de cadastro Link Cadastro.html ligado

---
+++ pagina.cadastro/cadastro.de.produto/js/script.js
@@ -0,0 +1,81 @@
+class Validatotr {
+
+    constructor(){
+        this.Validatios = [
+            "data-min-length",
+        ]
+    }
+
+    //iniciar a validação de todos os campos
+    validate(form ) {
+        //pegar os inputs
+        let inputs =  form.getElementsByTagName('input');
+
+        //transformo o HTMLColletion -> array
+        let inputsArray = [...inputs];
+
+        //loop nos inputs e validação mediante ao que for encontrado
+        inputsArray.forEach(function(input) {
+           
+        //loop em todos as validações existentes
+        for(let i=0; this.Validatios.length > i; i++) {
+            //verifica se a validação atual existe no input
+            if(input.getAttribute(this.Validatios[i]) !=null) {
+
+                //limpando a string para  virar um método
+                let method = this.validatios[i].replace('data', '').replace('-','');
+
+                //valor do input
+                let value = input.getAttribute(this.validations[i]);
+
+                //invocar o método
+                this[method](input, value);
+               
+            }
+        }
+        }, this );
+    }
+    //verifica se um input possui um número mínimo de caracteres
+    minlenght(input, minValue){
+
+        let inputLength = input.valur.length;
+
+        let errorMessage = 'O campo precisa ter pelo menos ${minValue} caracteres';
+
+        if(inputLength < minValue) {
+            this.printMesssage(input, errorMessage);
+        }
+    }
+    
+    // método para imprimir mensagens de erro
+    printMesssage(input, msg) {
+
+        let template = docmuent.querySelection('.error-validation').cloneNode(true);
+
+        template.textContent = msg;
+
+        let imputParent = input.parentNode;
+
+        template.classList.remove('template');
+
+        inputParent.appendChild(template);
+
+    }
+
+}
+
+
+let form = document.getElementById("register-form")
+let submit = document.getElementById("btn-submit")
+
+let Validator = new Validator();
+
+submit.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    console.log('funcionou');
+
+    validator.validate(form);
+
+});
