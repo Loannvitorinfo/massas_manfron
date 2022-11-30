30c84cb50983786e32c6a5346f2c9600558af5e2
loannvitorinfo <loannvitor@gmail.com>
2022-11-30 15:30:05 -0300

    Organização do projeto

--- IMAGENS/CSS/Javascript.js
+++
@@ -1,8 +0,0 @@
-var imagens = ["img01.png", "img02.png", "img03.png", "img04.png"];
-var imagematual = 0;
-
-function trocaimagem() {
- imagematual = (imagematual + 1) % 4;
-document.querySelector('.gif img'). src = imagens[imagematual];
-}
-setInterval(trocaimagem, 1500);
--- README.md
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-# massas_manfron
-Massas_Manfron
-=======
-# massas_manfron
->>>>>>> 80d7177551b3fd9a67b6d787f377214892be02e6
